2425b58d76cd5c3aaf3388e51e48abc2d980601c
vinothini <vinothinielangovan8@gmail.com>
2024-08-07 16:59:14 +0530

    EMS1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Employee } from './model/Employee';
+import { EmployeeService } from './employee.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,47 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Ems';
+  employee : Employee;
+  result : string;
+  employeeArr : Employee[];
+  flag : boolean;
+  
+
+  constructor(private service : EmployeeService){
+    this.employee = new Employee();
+    this.result =" ";
+    this.employeeArr = [];
+    this.flag = false;
+  }
+
+  insertEmployee(data : any) {
+    this.employee.id = data.empId;
+    this.employee.empName = data.empName;
+    this.employee.empSalary = data.empSalary;
+    alert(data.empId+" "+data.empName+" "+data.empSalary);
+    this.result = this.service.insertEmployee(this.employee);
+  }
+
+  updateEmployee(data : any){
+    this.employee.id = data.empId;
+    this.employee.empName = data.empName;
+    this.employee.empSalary = data.empSalary;
+    alert(data.empId+" "+data.empName+" "+data.empSalary);
+    this.result = this.service.updateEmployee(this.employee);
+
+  }
+
+  deleteEmployee(data : any){
+    this.result = this.service.deleteEmployee(data.empId);
+  }
+
+  findEmployee(data : any){
+    this.employee =this.service.findEmployee(data.empId);
+    this.result = this.employee.id + " " + this.employee.empName + " " + this.employee.empSalary;
+  }
+
+  findAllEmployee(){
+   this.employeeArr = this.service.findAllEmployee();
+   this.flag = true;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { EmployeeService } from './employee.service';
+import { HttpClientModule, provideHttpClient, withFetch } from '@angular/common/http';
+
+
 
 @NgModule({
   declarations: [
@@ -10,10 +15,16 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+    
+    
   ],
   providers: [
-    provideClientHydration()
+    EmployeeService,
+    provideHttpClient(withFetch())
+
   ],
   bootstrap: [AppComponent]
 })
